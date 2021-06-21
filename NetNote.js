a55f9f51f3654d82e734d5c73e819ffa4b7ae85a
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2021-06-21 13:44:24 -0300

    Update NetNote.js

--- NetNote.js
+++ NetNote.js
@@ -1,21 +1,19 @@
 let notasAlunoA = { 'Português': [ 10, 3, 6, 6 ],
-									'Matemática': [5.5, 6.0, 6, 5],
-									'História': [7.5, 6.5, 10, 8.5]
-								};
-
+		'Matemática': [5.5, 6.0, 6, 5],
+		'História': [7.5, 6.5, 10, 8.5]
+		};
 
 let calcNote = (materias) => {    
-		var calc = materias[ 0 ] + materias[ 1 ] + materias[ 2 ] + materias[ 3 ]        
+	var calc = materias[ 0 ] + materias[ 1 ] + materias[ 2 ] + materias[ 3 ]
+	console.log( "Cauculando notas..." )
 
-		console.log( "Cauculando notas..." )
-
-		if ( calc >= 22.5 ){
-				console.log( "Parabens pirralho foi aprovado!" )
-				console.log( 'Pontuação:', calc )
-		}else{
-				console.log( "Reprovado pivete!  ")
-				console.log( 'Pontuação:', calc )
-		};
+	if ( calc >= 22.5 ){
+			console.log( "Parabens pirralho foi aprovado!" )
+			console.log( 'Pontuação:', calc )
+	}else{
+			console.log( "Reprovado pivete!  ")
+			console.log( 'Pontuação:', calc )
+	};
 };
 
 calcNote(notasAlunoA.Português)
