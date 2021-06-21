8d54d911fbe24492828f45a4c0bd2eefb2f22a0b
c4l4ng0_m4t4d0r <40896649+D4rks1R3@users.noreply.github.com>
2021-06-21 13:42:17 -0300

    Primary Commit
    Update code - 1

---
+++ NetNote.js
@@ -0,0 +1,25 @@
+let notasAlunoA = { 'Português': [ 10, 3, 6, 6 ],
+									'Matemática': [5.5, 6.0, 6, 5],
+									'História': [7.5, 6.5, 10, 8.5]
+								};
+
+
+let calcNote = (materias) => {    
+		var calc = materias[ 0 ] + materias[ 1 ] + materias[ 2 ] + materias[ 3 ]        
+
+		console.log( "Cauculando notas..." )
+
+		if ( calc >= 22.5 ){
+				console.log( "Parabens pirralho foi aprovado!" )
+				console.log( 'Pontuação:', calc )
+		}else{
+				console.log( "Reprovado pivete!  ")
+				console.log( 'Pontuação:', calc )
+		};
+};
+
+calcNote(notasAlunoA.Português)
+
+calcNote(notasAlunoA.Matemática)
+
+calcNote(notasAlunoA.História)
